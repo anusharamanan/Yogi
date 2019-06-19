c22553a8f0251a31da57c315e5870d0b8bc89e0f
anusharamanan <51978153+anusharamanan@users.noreply.github.com>
2019-06-19 16:20:43 +1000

    Update searchelement.py

--- searchelement.py
+++ searchelement.py
@@ -1,11 +1,17 @@
 # Search element in a list
 import random
 listn=sorted(random.sample(range(100),10))
-num=input("Enter a number to be searched in sorted list: ")
-
 print(listn)
+num=int(input("Enter a number to be searched in list: "))
 
-if num in listn:
-    print(num+" is present in list")
+def search(listn,num):
+    for n in listn:
+        if n == num:
+            return True
+            
+    return False
+
+if search(listn,num)==True:
+    print("Number is in list")
 else:
-    print(num+" is not present in list")
+    print("Number is not in list")
